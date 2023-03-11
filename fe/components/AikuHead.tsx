38ed63bb89c244ac8510fbfed9fedb242f1a1695
Param Singh <me@param.codes>
2023-03-11 22:30:07 +0530

    Add robots and sitemap

--- fe/components/AikuHead.tsx
+++ fe/components/AikuHead.tsx
@@ -1,16 +1,22 @@
 import Head from "next/head";
 
-
-const AikuHead = () => {
-    return (
+const AikuHead = ({ canonical = "" }: { canonical?: string }) => {
+  return (
     <Head>
-        <title>Aiku: The AI Haiku writer</title>
-        <meta name="description" content="An AI that can write poems for you." />
-        <link rel="icon" href="/logo.ico" />
-        <script data-goatcounter="https://aiku.goatcounter.com/count"
-        async src="//gc.zgo.at/count.js"></script>
-      </Head>
-    );
-}
+      <title>Aiku: The AI Haiku writer</title>
+      <meta
+        name="description"
+        content="Discover Aiku, an artificial intelligence that loves poetry. Aiku can write poems in different forms and genres, from love to horror. Explore https://aiku.param.codes to see what Aiku can do."
+      />
+      <link rel="icon" href="/logo.ico" />
+      <script
+        data-goatcounter="https://aiku.goatcounter.com/count"
+        async
+        src="//gc.zgo.at/count.js"
+      ></script>
+      <link rel="canonical" href={`https://aiku.param.codes/${canonical}`} />
+    </Head>
+  );
+};
 
 export { AikuHead };
